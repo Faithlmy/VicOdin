ab8f1b275ff3c735dc69a191a199cb29660de203
Faithlmy <lmengyy@126.com>
2018-08-01 16:34:57 +0800

    modified CommonMapper.java and TestDataMapper.java

--- src/main/java/com/vic/rest/mapper/odin/CommonMapper.java
+++ src/main/java/com/vic/rest/mapper/odin/CommonMapper.java
@@ -3,7 +3,6 @@ package com.vic.rest.mapper.odin;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public interface CommonMapper {
 
--- src/main/java/com/vic/rest/mapper/otd/TestDataMapper.java
+++ src/main/java/com/vic/rest/mapper/otd/TestDataMapper.java
@@ -4,17 +4,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * ======================================================================
- * 版權：富士康工業互聯網股份有限公司 平臺解決方案事業處 版權所有 (c) 2018
- * 文件：com.odin.rest.mapper
- * 所含類：TestDataMapper
- * 修改記錄
- * 日期				作者					版本				內容
- * ======================================================================
- * 2018/7/25		yq.xiao(F1333250)	    V1.0			新建
- * ======================================================================
- */
 public interface TestDataMapper {
     List<Map> selectBySql(String sql);
     
