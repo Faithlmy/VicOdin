2d2a23448dc087a0296705aeb15c25c33d2c7462
Faithlmy <lmengyy@126.com>
2018-08-09 17:02:29 +0800

    acuserresp

---
+++ src/main/java/com/vic/rest/vo/ACUserResp.java
@@ -0,0 +1,37 @@
+
+package com.vic.rest.vo;
+
+
+public class ACUserResp {
+
+	private String status;
+	
+	private String msg;
+	
+	private ACUser data;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public ACUser getData() {
+		return data;
+	}
+
+	public void setData(ACUser data) {
+		this.data = data;
+	}
+	
+}
