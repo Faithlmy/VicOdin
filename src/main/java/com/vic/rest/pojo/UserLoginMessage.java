9a3822a71c593e39553c2b762fb48d25cd9ab4ad
Faithlmy <lmengyy@126.com>
2018-08-21 18:57:54 +0800

    pojo

---
+++ src/main/java/com/vic/rest/pojo/UserLoginMessage.java
@@ -0,0 +1,35 @@
+package com.vic.rest.pojo;
+
+import java.util.Date;
+
+public class UserLoginMessage {
+    private Integer id;
+
+    private String username;
+
+    private Date loginTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+}
